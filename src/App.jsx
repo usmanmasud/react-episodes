9f542417b15a55fd4ac816470dc08046b5e74729
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-11-05 12:38:43 +0100

    star-rating

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import Accordian from "./accordian/Page"
-import RandomColor from "./random-color/page"
+// import RandomColor from "./random-color/page"
+import { StarRating } from "./star-rating/page"
 
 function App() {
   return (
     <>
       {/* <Accordian /> */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+      <StarRating />
     </>
   )
 }
