e73e526d610df2dba32319e807748c74c70f99ba
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-11-10 15:17:25 +0100

    finished theme switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 // import Accordian from "./accordian/Page"
 // import RandomColor from "./random-color/page"
 
-import ImageSlider from "./ImageSlider/Page"
-import LoadMoreData from "./load-more-btm/page"
-import QrCodeGenerator from "./qr-generator/page"
-import Page from "./star-rating/page"
-import menus from "./Tree-view/data"
-import TreeView from "./Tree-view/page"
+import './App.css'
+
+import ThemeSwitch from "./theme-switch/page"
+
+// import ImageSlider from "./ImageSlider/Page"
+// import LoadMoreData from "./load-more-btm/page"
+// import QrCodeGenerator from "./qr-generator/page"
+// import Page from "./star-rating/page"
+// import menus from "./Tree-view/data"
+// import TreeView from "./Tree-view/page"
 
 function App() {
   return (
@@ -24,7 +28,9 @@ function App() {
 
       {/* <TreeView menus={menus} /> */}
 
-      <QrCodeGenerator />
+      {/* <QrCodeGenerator /> */}
+
+      <ThemeSwitch />
 
     </>
   )
