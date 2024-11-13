355896faa1933071f533d0339b97714d8af6cd11
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-11-13 23:48:32 +0100

    start custom tabs

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,10 @@ function App() {
 
       {/* <ThemeSwitch /> */}
 
-      <ScrollIndicator url={'https://dummyjson.com/products?limit-100'} />
+      {/* <ScrollIndicator url={'https://dummyjson.com/products?limit-100'} /> */}
+
+
+
 
     </>
   )
